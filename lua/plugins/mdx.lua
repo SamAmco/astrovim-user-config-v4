786f51731c93141a405c1150144ee542caac2b4d
Sam Harvey-Collins <sam.harvey-collins@deliveroo.co.uk>
2024-12-05 15:26:37 +0000

    Add mdx support

---
+++ lua/plugins/mdx.lua
@@ -0,0 +1,13 @@
+-- Enable basic MDX support
+
+---@type LazySpec
+return {
+  "davidmh/mdx.nvim",
+  config = true,
+}
+
+-- {
+--   "davidmh/mdx.nvim",
+--   config = true,
+--   dependencies = {"nvim-treesitter/nvim-treesitter"}
+-- }
