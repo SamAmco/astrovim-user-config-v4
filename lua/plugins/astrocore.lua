8b41af84a59a3f126580e92d270c8bd9005216f9
Sam <samuel.amco@gmail.com>
2025-01-31 23:50:42 +0000

    Add avante plugin

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -73,15 +73,15 @@ return {
           desc = "Close last window",
         },
         ["<Leader>a"] = { desc = "AI Tools" },
-        ["<Leader>at"] = {
-          ":CopilotChatToggle<cr>",
-          desc = "Toggle Copilot Chat",
-        },
         -- tables with the `name` key will be registered with which-key if it's installed
         -- this is useful for naming menus
         ["<Leader>b"] = { name = "Buffers" },
         -- quick save
         -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+      },
+
+      t = {
+        ["kj"] = "<C-\\><C-n>"
       },
 
       i = {
